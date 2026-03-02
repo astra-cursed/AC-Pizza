1d9f7b7b2b342213b9979f26f027ec94404805b2
astra-cursed <gradus.gridasov@gmail.com>
2026-03-02 14:04:39 +0300

    Подправил проверку верификации юзера

--- app/actions.ts
+++ app/actions.ts
@@ -156,37 +156,37 @@ export async function registerUser(body: Prisma.UserCreateInput) {
     });
 
     if (user) {
-      if (!user.verified) {
-        throw new Error('Почта не подтверждена');
-      }
+      // if (!user.verified) {
+      //   throw new Error('Почта не подтверждена');
+      // }
 
       throw new Error('Пользователь уже существует');
     }
 
-    const createdUser = await prisma.user.create({
-      data: {
-        fullName: body.fullName,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-      },
-    });
+    // const createdUser = await prisma.user.create({
+    //   data: {
+    //     fullName: body.fullName,
+    //     email: body.email,
+    //     password: bcrypt.hashSync(body.password, 10),
+    //   },
+    // });
 
-    const code = Math.floor(100000 + Math.random() * 900000).toString();
+    // const code = Math.floor(100000 + Math.random() * 900000).toString();
 
-    await prisma.verificationCode.create({
-      data: {
-        code,
-        userId: createdUser.id,
-      },
-    });
+    // await prisma.verificationCode.create({
+    //   data: {
+    //     code,
+    //     userId: createdUser.id,
+    //   },
+    // });
 
-    await sendEmail(
-      createdUser.email,
-      'Next Pizza / 📝 Подтверждение регистрации',
-      VerificationUserTemplate({
-        code,
-      }),
-    );
+    // await sendEmail(
+    //   createdUser.email,
+    //   'Next Pizza / 📝 Подтверждение регистрации',
+    //   VerificationUserTemplate({
+    //     code,
+    //   }),
+    // );
   } catch (err) {
     console.log('Error [CREATE_USER]', err);
     throw err;
